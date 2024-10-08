45e20ef646c4322632a1e7df9cfe82796891bc72
markbus-ai <markbustos@hotmail.com>
2024-10-07 23:44:07 -0300

    configurando script main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import subprocess
+import os
+if __name__ == "__main__":
+    if not os.listdir("tmp"):
+        subprocess.Popen(["python", "Front/crear_mesa.py"])
+    subprocess.Popen(["python", "pc_front.py"])
+    subprocess.Popen(["python", "Back/Api.py"]) 
